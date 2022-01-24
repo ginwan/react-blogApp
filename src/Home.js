a3d6c378d02f51fd979bfaf03fc7534249abf347
ginwan <ginwan697@gmail.com>
2022-01-24 19:01:21 +0200

    try use effect hooks

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react'
+import Bloglist from './Bloglist';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'ginwan', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'amar', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'ayla', id: 3 }
+    ])
+    const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog => blog.id !== id) 
+        setBlogs(newBlogs)
+    }
+    useEffect(()=>{
+        console.log("use effect ran")
+        console.log(blogs)
+    })
     return ( 
         <div className="home">
-            <h1>Home page</h1>
+            <Bloglist blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
         </div>
      );
 }
